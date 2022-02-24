72f57f99594742b21040d8d4c6d9fc7a93eb8d7a
ThijmenJava <94443778+ThijmenJava@users.noreply.github.com>
2022-02-24 16:50:50 +0100

    pizza.glb toegevoegt aan de index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +6,35 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Home</title>
 </head>
 <body>
-
+    <main class="mainloc">
+        <div class="textpizza center">
+            <h1 class="maintext"> Pizzeria Da La Thijma</h1>
+        </div>
+        <div class="center model">
+            <model-viewer bounds="tight" src="fotos/pizza.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls environment-image="neutral" poster="poster.webp" shadow-intensity="0.75">
+                <div class="progress-bar hide" slot="progress-bar">
+                    <div class="update-bar"></div>
+                </div>
+            </model-viewer>
+        </div>
+        <div class="mainpage center_boven">
+            <div class="buttonbox">
+                <a href="menu.php"><button class="buttonhome">Menu</button></a>
+                <button class="buttonhome">Login</button>
+            </div>
+        </div>
+    </main>
+    <script src="js/main.js"></script>
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
 </body>
 </html>
